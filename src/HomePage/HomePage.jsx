a16c4c419a305e1bca30c510bdb0e388ed8b104d
iiceywill <will.cao@smoothstack.com>
2020-06-12 21:15:01 -0400

    remove link in home

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { userActions } from "../_actions";
 import Pay from "./components/Pay";
