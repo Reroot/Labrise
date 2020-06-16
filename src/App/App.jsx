1bd583324991edadb59e86b1f642e5a02d2a7262
iiceywill <will.cao@smoothstack.com>
2020-06-16 02:27:22 -0400

    moved header to it's own component out from App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 
 import { history } from "../_helpers";
 import { alertActions } from "../_actions";
@@ -10,8 +9,7 @@ import { HomePage } from "../HomePage";
 import { LoginPage } from "../LoginPage";
 import { RegisterPage } from "../RegisterPage";
 import { PatientPage } from "../PatientPage/PatientPage";
-import Grid from "@material-ui/core/Grid";
-import { ButtonGroup, Button } from "@material-ui/core";
+import { Header } from "../_components";
 
 function App() {
   const alert = useSelector((state) => state.alert);
@@ -43,48 +41,7 @@ function App() {
         {/*
         Need to make this more fluid with pending before displaying
         */}
-        {user && (
-          <Grid
-            container
-            id="headerGrid"
-            direction="row"
-            style={{ height: "10vh" }}
-          >
-            <Grid item xs={3}>
-              <h1>Hi {user.firstName}!</h1>
-            </Grid>
-            <Grid item xs={7}>
-              <Grid item>
-                <ButtonGroup
-                  variant="contained"
-                  color="primary"
-                  aria-label="contained primary button group"
-                >
-                  <Button>
-                    <Link to="/" style={{ color: "black" }}>
-                      Home
-                    </Link>
-                  </Button>
-                  <Button>
-                    <Link to="/5" style={{ color: "black" }}>
-                      Two
-                    </Link>
-                  </Button>
-                  <Button>
-                    <Link to="/labresults" style={{ color: "black" }}>
-                      Lab results
-                    </Link>
-                  </Button>
-                </ButtonGroup>
-              </Grid>
-            </Grid>
-            <Grid item xs={2} style={{ textAlign: "right" }}>
-              <Button variant="outlined" color="primary">
-                <Link to="/login">Logout</Link>
-              </Button>
-            </Grid>
-          </Grid>
-        )}
+        {user && <Header />}
         <Switch>
           <PrivateRoute exact path="/" component={HomePage} />
           <Route path="/login" component={LoginPage} />
---
+++ src/_components/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { ButtonGroup, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function Header() {
+  const user = useSelector((state) => state.authentication.user);
+  return (
+    <Grid container id="headerGrid" direction="row" style={{ height: "10vh" }}>
+      <Grid item xs={3}>
+        <h1>Hi {user.firstName}!</h1>
+      </Grid>
+      <Grid item xs={7}>
+        <Grid item>
+          <ButtonGroup
+            variant="contained"
+            color="primary"
+            aria-label="contained primary button group"
+          >
+            <Button>
+              <Link to="/" style={{ color: "black" }}>
+                Home
+              </Link>
+            </Button>
+            <Button>
+              <Link to="/5" style={{ color: "black" }}>
+                Two
+              </Link>
+            </Button>
+            <Button>
+              <Link to="/labresults" style={{ color: "black" }}>
+                Lab results
+              </Link>
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+      <Grid item xs={2} style={{ textAlign: "right" }}>
+        <Button variant="outlined" color="primary">
+          <Link to="/login">Logout</Link>
+        </Button>
+      </Grid>
+    </Grid>
+  );
+}
+
+export { Header };
