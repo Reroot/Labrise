cc80259fd7230b9646aca62ba2e232ad312fea07
iiceywill <will.cao@smoothstack.com>
2020-06-16 23:45:06 -0400

    made basic template from homeAction file

---
+++ src/_actions/homeActions.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+import { homeConstants } from "../_constants";
+
+import {} from "";
--- src/_actions/index.js
+++ src/_actions/index.js
@@ -1,3 +1,4 @@
-export * from './alert.actions';
-export * from './user.actions';
-export * from './invoiceActions';
+export * from "./alert.actions";
+export * from "./user.actions";
+export * from "./invoiceActions";
+export * from "./homeContent";
