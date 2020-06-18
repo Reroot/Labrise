16bd1ffea9ac8d5c353f9f5a3ab4a7ba975d39e6
iiceywill <will.cao@smoothstack.com>
2020-06-18 17:22:42 -0400

    Added more textfields to profile

--- src/Style/profilestyle.js
+++ src/Style/profilestyle.js
@@ -1,6 +1,11 @@
 import { makeStyles } from "@material-ui/core/styles";
+
 const ProfileStyles = makeStyles({
-  textField: { outline: "solid", outlineColor: "blue" },
+  textField: {
+    outline: "solid",
+    outlineColor: "blue",
+    margin: ".6rem",
+  },
 });
 
 export { ProfileStyles };
--- src/_components/profile.jsx
+++ src/_components/profile.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import TextField from "@material-ui/core/TextField";
 import { ProfileStyles } from "../Style/profilestyle";
+import InputAdornment from "@material-ui/core/InputAdornment";
 function ProfilePage() {
   const useStyles = ProfileStyles();
   const loggingIn = useSelector((state) => state.authentication.loggingIn);
@@ -11,8 +12,7 @@ function ProfilePage() {
   // reset login status
   useEffect(() => {}, []);
   return (
-    <Container maxWidth="xs">
-      <title>Please enter your log in or register into our Secure Portal</title>
+    <Container>
       <TextField
         variant="filled"
         margin="normal"
@@ -29,12 +29,46 @@ function ProfilePage() {
       <TextField
         variant="outlined"
         margin="normal"
-        required
-        name="password"
-        id="passwordID"
-        label="Password"
-        type="password"
+        name="firstName"
+        id="firstNameID"
+        label="First Name"
+        type="text"
         className={useStyles.textField}
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        name="lastName"
+        id="lastNameID"
+        label="Last Name"
+        type="text"
+        className={useStyles.textField}
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        name="mobilePhone"
+        id="mobilePhoneID"
+        label="Mobile Phone"
+        type="text"
+        className={useStyles.textField}
+      />
+      <TextField
+        variant="outlined"
+        margin="normal"
+        name="emailAddress"
+        id="emailAddressID"
+        label="Email Address"
+        type="text"
+        className={useStyles.textField}
+      />
+      <TextField
+        label="Weight"
+        id="filled-start-adornment"
+        InputProps={{
+          endAdornment: <InputAdornment position="start">Kg</InputAdornment>,
+        }}
+        variant="filled"
       />
     </Container>
   );
