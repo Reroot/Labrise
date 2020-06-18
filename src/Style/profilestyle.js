470008cd11ac2595d99f6e7447d4c93f7ba05962
iiceywill <will.cao@smoothstack.com>
2020-06-18 18:44:50 -0400

    fixed profile data to not display until done.

--- src/Style/profilestyle.js
+++ src/Style/profilestyle.js
@@ -2,8 +2,6 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const ProfileStyles = makeStyles({
   textField: {
-    outline: "solid",
-    outlineColor: "blue",
     margin: ".6rem",
   },
   container: {
--- src/_components/profile.jsx
+++ src/_components/profile.jsx
@@ -4,85 +4,78 @@ import { useDispatch, useSelector } from "react-redux";
 import TextField from "@material-ui/core/TextField";
 import { ProfileStyles } from "../Style/profilestyle";
 import InputAdornment from "@material-ui/core/InputAdornment";
+import * as profileActions from "../_actions/profile-actions";
+
 function ProfilePage() {
   const useStyles = ProfileStyles();
-  const loggingIn = useSelector((state) => state.authentication.loggingIn);
   const dispatch = useDispatch();
   let info = useSelector((state) => state.profileReducer.profileData);
   // reset login status
   console.log(info);
-  console.log(info.pData.value[0]["firstname"]);
-  useEffect(() => {}, []);
-  return (
-    <Container className={useStyles.container}>
-      <TextField
-        variant="filled"
-        margin="normal"
-        InputProps={{
-          readOnly: true,
-        }}
-        defaultValue="Username Here"
-        name="username"
-        id="userID"
-        label="Username"
-        autoFocus
-        className={useStyles.textField}
-      />
-      <TextField
-        variant="outlined"
-        margin="normal"
-        name="firstName"
-        id="firstNameID"
-        value={info.pData.value[0]["firstname"]}
-        label="First Name"
-        type="text"
-        className={useStyles.textField}
-        autoComplete="off"
-      />
-      <TextField
-        variant="outlined"
-        margin="normal"
-        name="lastName"
-        id="lastNameID"
-        value={info.pData.value[0]["lastname"]}
-        label="Last Name"
-        type="text"
-        autoComplete="off"
-        className={useStyles.textField}
-      />
-      <TextField
-        variant="outlined"
-        margin="normal"
-        name="mobilePhone"
-        id="mobilePhoneID"
-        value={info.pData.value[0]["mobilephone"]}
-        label="Mobile Phone"
-        type="text"
-        autoComplete="off"
-        className={useStyles.textField}
-      />
-      <TextField
-        variant="outlined"
-        margin="normal"
-        name="emailAddress"
-        id="emailAddressID"
-        value={info.pData.value[0]["emailaddress1"]}
-        label="Email Address"
-        type="text"
-        className={useStyles.textField}
-      />
-      <TextField
-        label="Weight"
-        id="filled-start-adornment"
-        InputProps={{
-          endAdornment: <InputAdornment position="start">Kg</InputAdornment>,
-        }}
-        value={info.pData.value[0]["new_weight"]}
-        variant="filled"
-        autoComplete="off"
-      />
-    </Container>
-  );
+  useEffect(() => {
+    dispatch(profileActions.readProfile());
+  }, []);
+  if (info && info.requestSuccessful) {
+    return (
+      <Container className={useStyles.container}>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          name="firstName"
+          id="firstNameID"
+          value={info.pData.value[0]["firstname"]}
+          label="First Name"
+          type="text"
+          className={useStyles.textField}
+          autoComplete="off"
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          name="lastName"
+          id="lastNameID"
+          value={info.pData.value[0]["lastname"]}
+          label="Last Name"
+          type="text"
+          autoComplete="off"
+          className={useStyles.textField}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          name="mobilePhone"
+          id="mobilePhoneID"
+          value={info.pData.value[0]["mobilephone"]}
+          label="Mobile Phone"
+          type="text"
+          autoComplete="off"
+          className={useStyles.textField}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          name="emailAddress"
+          id="emailAddressID"
+          value={info.pData.value[0]["emailaddress1"]}
+          label="Email Address"
+          type="text"
+          className={useStyles.textField}
+        />
+        <TextField
+          label="Weight"
+          id="filled-start-adornment"
+          InputProps={{
+            endAdornment: <InputAdornment position="start">Kg</InputAdornment>,
+          }}
+          value={info.pData.value[0]["new_weight"]}
+          variant="filled"
+          autoComplete="off"
+        />
+      </Container>
+    );
+  } else {
+    return "";
+  }
 }
 
 export { ProfilePage };
